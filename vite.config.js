4a574008e87321748ee8693db4564ec0d35d7013
ykkhmrdn <ykkhmrdn@gmail.com>
2024-07-07 22:39:46 +0700

    chore: Update npm dependency to latest stable version

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import Checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [react(), Checker({ typescript: true })],
+  plugins: [react(), Checker({ typescript: false })],
 });
