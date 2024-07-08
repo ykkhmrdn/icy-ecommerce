09af8c42607d8b9d47e4c0202c58d26a6332ca58
ykkhmrdn <ykkhmrdn@gmail.com>
2024-07-08 18:34:13 +0700

    chore: Update image paths in ProductLayouts.jsx

--- src/components/Layouts/ProductLayouts.jsx
+++ src/components/Layouts/ProductLayouts.jsx
@@ -1,48 +1,49 @@
 import { ProductListCard} from "../Elements/Products/ProductsCard";
 
-const CONTENTS = [
+const products = [
   {
-    img: "/assets/img/Nike Dunk.jpg",
+    img: "/images/Nike Dunk.jpg",
     name: "Nike Dunk Low Retro White Black",
     price: "Rp 1,300,000",
   },
   {
-    img: "/assets/img/Nike Air Jordan.jpg",
+    img: "/images/Nike Air Jordan.jpg",
     name: "Nike Air Jordan Mid Red Panda",
     price: "Rp 1,800,000",
   },
   {
-    img: "/assets/img/Nike Cortez.jpg",
+    img: "/images/Nike Cortez.jpg",
     name: "Nike Cortez Classic Leather",
     price: "Rp 1,600,000",
   },
   {
-    img: "/assets/img/Nike Mid Blazer.jpg",
+    img: "/images/Nike Mid Blazer.jpg",
     name: "Nike Blazer Mid '77 Vintage",
     price: "Rp 1,300,000",
   },
   {
-    img: "/assets/img/Nike Defy.jpg",
+    img: "/images/Nike Defy.jpg",
     name: "Nike Defy All Day",
     price: "Rp 700,000",
   },
   {
-    img: "/assets/img/Nike Air Force.jpg",
+    img: "/images/Nike Air Force.jpg",
     name: "Nike Air Force 1 Triple White",
     price: "Rp 1,350,000",
   },
 ];
 
-const ProductLayout = ({ children }) => {
+const ProductLayout = () => {
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2">
-        {CONTENTS.map(({ img, name, price }, index) => (
+        {products.map(({ img, name, price }, index) => (
           <ProductListCard key={index} img={img} name={name} price={price}/>
         ))}
       </div>
     </div>
   );
 };
+
 
 export default ProductLayout;
